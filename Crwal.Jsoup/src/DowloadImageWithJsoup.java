895e0fc12196767c214bf2b51b77044c77f37343
unless003 <unless003@gmail.com>
2019-08-30 17:35:47 +0700

    no message

--- Crwal.Jsoup/src/DowloadImageWithJsoup.java
+++ Crwal.Jsoup/src/DowloadImageWithJsoup.java
@@ -1,37 +1,55 @@
-import java.io.File;
+	import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.Path;
+
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-public class DowloadImageWithJsoup {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Document doc;
+	import org.jsoup.nodes.Document;
 
-		try {
-			doc = Jsoup.connect("https://www.zomato.com/pune/bubble-bee-aundh/photos").get();	// URL from where you want to collect photos
-			Elements images = doc.select(".photos_container_load_more a img");					// Simple selector to spot all img tags in html Doc
-			for (Element image : images) {					
-				//System.out.println("src : " + image.attr("data-original").split("\\?")[0]);
-				String imgURL = image.attr("data-original").split("\\?")[0];					// Collecting data-original(or src) attribute of img tag (I have split it on ? so as to remove extra URL parameters such as fit=, crop=)
-				new File("E:/CrawlerImages").mkdirs();											// Create new directory in you system where you want to store images
-				URL url = new URL(imgURL);														
-				InputStream in = url.openConnection().getInputStream();			
-				Files.copy(in, Paths.get("E:/CrawlerImages/" + imgURL.substring(imgURL.lastIndexOf("/"))), // Save image in created folder with some name
-						StandardCopyOption.REPLACE_EXISTING);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	import org.jsoup.nodes.Element;
+
+	import org.jsoup.select.Elements;
+	
+
+	
+	public class DowloadImageWithJsoup {
+
+
+
+
+
+	    public static void main(String[] args) {
+	    	
+
+
+	    		
+	        Document doc;
+
+	        try {
+
+
+	            // get all images
+
+	            doc = Jsoup.connect("https://sachvui.com/doc-sach/tham-tu-lung-danh-conan-ban-dep-gosho-aoyama/chap-1.html").get();
+
+	            Elements images = doc.body().select("img[src~=(?i)\\.(png|jpe?g|gif)]") ;
+	     //       doc.toOutputStream(new FileOutputStream("/home/user/filepath/filename.html"), "UTF-8");
+	            
+	       //     File f = new File("conan1.html");
+	            
+	        //    doc.body();
+	            
+	            System.out.println(images);
+	        } catch (IOException e) {
+
+	            e.printStackTrace();
+
+	        }
+
+
+	    }
 
 	}
-	}
-
-}
